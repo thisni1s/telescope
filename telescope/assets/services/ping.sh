704bd19b9abb14e366e5c930cb82a25347b823bb
Nils Kempen <nils.kempen@jn2p.de>
2024-12-10 14:24:58 +0100

    features

---
+++ telescope/assets/services/ping.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo '{"ping": "sucess"}'
--- telescope/assets/startup.sh
+++ telescope/assets/startup.sh
@@ -4,8 +4,16 @@ export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt upgrade -y
 apt install tcpdump -y # already installed
-wget https://github.com/thisni1s/telescope/raw/refs/heads/main/telescope/tcpdumpd.service -P /usr/lib/systemd/system
-wget https://github.com/thisni1s/telescope/raw/refs/heads/main/telescope/upload.sh -P /root
+
+mkdir -p /var/scripts
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/services/tcpdumpd.service -P /usr/lib/systemd/system
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/services/webhook.service -P /usr/lib/systemd/system
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/services/webhook.socket -P /usr/lib/systemd/system
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/upload.sh -P /var/scripts/
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/services/teardown.sh -P /var/scripts/
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/services/ping.sh -P /var/scripts/
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/services/restart.sh -P /var/scripts/
+wget https://raw.githubusercontent.com/thisni1s/telescope/refs/heads/main/telescope/assets/services/webhook.json -P /etc/webhook.json
 systemctl enable tcpdumpd
 
 #Install Minio Client
@@ -22,6 +30,8 @@ curl -L https://github.com/adnanh/webhook/releases/latest/download/webhook-linux
 tar xf webhook.tar.gz
 mv webhook-linux-amd64/webhook /usr/local/bin/webhook
 rmdir webhook-linux-amd64
+systemctl enable webhook.socket
+systemctl start webhook.socket
 
 # Change SSH Port, Ubuntu has socket based activation so it needs to be set like this
 mkdir -p /etc/systemd/system/ssh.socket.d
@@ -40,4 +50,3 @@ bucket=$(cat /root/config/bucket.txt)
 mc alias set tupload $(cat /root/config/storageLoc.txt) $(cat /root/config/storageAccKey.txt) $(cat /root/config/storageSecKey.txt)
 
 systemctl start tcpdumpd
-reboot
