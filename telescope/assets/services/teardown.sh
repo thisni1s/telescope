3014005c86736e16eecf41d46c15092c48e5dd5d
Nils Kempen <nils.kempen@jn2p.de>
2025-01-14 17:09:42 +0100

    fix descriptor creation

--- telescope/assets/services/teardown.sh
+++ telescope/assets/services/teardown.sh
@@ -3,7 +3,8 @@ echo "started" > /root/config/teardownState.txt
 sh /var/scripts/upload.sh > /dev/null 2>&1
 systemctl stop tcpdumpd > /dev/null 2>&1
 desc=$(ls /root/config | grep descriptor)
-jq --arg deletion "$(date --iso-8601=seconds)" '. + {deletion: $deletion}' /root/config/$desc > /root/config/$desc
+jq --arg deletion "$(date --iso-8601=seconds)" '. + {deletion: $deletion}' /root/config/$desc > /root/config/temp.txt
+mv /root/config/temp.txt /root/config/$desc
 ip=$( cat /root/config/ip4.txt | sed -r 's/\./-/g' )
 mc cp /root/config/$desc tupload/$(cat /root/config/bucket.txt)/$ip/$desc
 sleep 5
