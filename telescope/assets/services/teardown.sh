0efa3aeab7c771f732e41a7e011d0560c50f8a35
Nils Kempen <nils.kempen@jn2p.de>
2025-01-14 16:17:43 +0100

    teardown time in descriptor

--- telescope/assets/services/teardown.sh
+++ telescope/assets/services/teardown.sh
@@ -2,5 +2,7 @@
 echo "started" > /root/config/teardownState.txt
 sh /var/scripts/upload.sh > /dev/null 2>&1
 systemctl stop tcpdumpd > /dev/null 2>&1
+jq --arg deletion "$(date --iso-8601=seconds)" '. + {deletion: $deletion}' /root/config/descriptor.txt > /root/config/descriptor.txt
+mc cp /root/config/descriptor.txt tupload/$(cat /root/config/bucket.txt)/$(cat /root/config/ip4.txt)/descriptor.txt
 echo "finished" > /root/config/teardownState.txt
 
