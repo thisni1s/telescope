38dec90e040d34a1c7b6d92f2e7f4a913b93c097
Nils Kempen <nils.kempen@jn2p.de>
2025-01-17 12:48:21 +0100

    uptime no more problems pls

--- telescope/assets/services/status.sh
+++ telescope/assets/services/status.sh
@@ -4,7 +4,7 @@
 SERVICES=("ssh.service" "ssh.socket" "webhook.service" "webhook.socket" "tcpdumpd.service")
 
 
-utime=$(uptime | jc --uptime)
+#utime=$(uptime | jc --uptime) # does not work for some obscure reason
 name=$(cat /etc/hostname)
 ip4=$(cat /root/config/ip4.txt)
 td=$(cat /root/config/teardownState.txt)
@@ -13,7 +13,8 @@ provider=$(cat /root/config/provider.txt)
 region=$(cat /root/config/region.txt)
 os=$(hostnamectl | grep Operating | cut -d ':' --fields 2 | tr -d ' ')
 
-status_json="{\"hostname\": \"$name\", \"uptime\": $utime, \"ipv4\": \"$ip4\",  \"teardown\": \"$td\", \"created\": \"$otime\", \"provider\": \"$provider\", \"region\": \"$region\", \"os\": \"$os\", "
+#status_json="{\"hostname\": \"$name\", \"uptime\": $utime, \"ipv4\": \"$ip4\",  \"teardown\": \"$td\", \"created\": \"$otime\", \"provider\": \"$provider\", \"region\": \"$region\", \"os\": \"$os\", "
+status_json="{\"hostname\": \"$name\", \"ipv4\": \"$ip4\",  \"teardown\": \"$td\", \"created\": \"$otime\", \"provider\": \"$provider\", \"region\": \"$region\", \"os\": \"$os\", "
 
 # Loop through each service and get its status
 for service in "${SERVICES[@]}"; do
