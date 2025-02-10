f07dea81308b14dd42a92b2708105c93fcfb8a33
nils <nkempen@uni-muenster.de>
2025-02-10 23:20:10 +0100

    idk

--- telescope/assets/startup.sh
+++ telescope/assets/startup.sh
@@ -13,7 +13,7 @@ openssl req -x509 -newkey ed25519 -keyout /root/config/key.key -outform PEM -out
 
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
-apt upgrade -y
+#apt upgrade -y
 
 # Install Corsaro
 curl https://pkg.caida.org/os/ubuntu/bootstrap.sh | bash
@@ -92,3 +92,4 @@ sudo iptables -A OUTPUT -o "$iface" -j DROP
 systemctl daemon-reload
 systemctl restart ssh.socket
 systemctl enable --now corsaro
+systemctl start corsaro
